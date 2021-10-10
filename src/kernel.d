2dd88af9f4457fc0a21d74cf80c6d627c3e2e489
Jakub Lecki <jakub.lecki@3mdeb.com>
2021-10-10 19:43:09 +0200

    boot first Rust bin
    Signed-off-by: Jakub Lecki <jakub.lecki@3mdeb.com>

--- src/kernel.d
+++
@@ -1,5 +0,0 @@
-kernel.o: kernel.c \
- /home/dev/opt/cross/lib/gcc/i686-elf/10.2.0/include/stdbool.h \
- /home/dev/opt/cross/lib/gcc/i686-elf/10.2.0/include/stddef.h \
- /home/dev/opt/cross/lib/gcc/i686-elf/10.2.0/include/stdint.h \
- /home/dev/opt/cross/lib/gcc/i686-elf/10.2.0/include/stdint-gcc.h

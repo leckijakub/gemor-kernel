2b7da0d813fba870bb7bc914e121378a977f69be
Jakub Lecki <lec.jakub@gmail.com>
2022-04-05 18:44:40 +0200

    src: main.rs: Detect and print memory regions.
    Signed-off-by: Jakub Lecki <lec.jakub@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 
 mod logger;
 mod vga_printer;
-use bootloader::{entry_point, BootInfo, boot_info::{MemoryRegion, MemoryRegions}};
-use core::{panic::PanicInfo, fmt::Result, ops::Deref};
+use bootloader::{entry_point, BootInfo, boot_info::{MemoryRegions}};
+use core::{panic::PanicInfo, ops::Deref};
 use logger::init_logger;
 
 const PANIC_HEADER: &str =
@@ -13,13 +13,19 @@ const PANIC_HEADER: &str =
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    let a = 5;
     // panic when framebuffer info couldn't be found
     let framebuffer = boot_info.framebuffer.as_mut().unwrap();
-
     let fb_info = framebuffer.info();
-    init_logger(framebuffer.buffer_mut(), fb_info);
 
+    init_logger(framebuffer.buffer_mut(), fb_info);
     log::info!("Hello World!");
+
+    check_memory(& boot_info.memory_regions);
+
+    log::info!("a address: {:p}", &a);
+    log::info!("kernel_main address: {:p}", &kernel_main);
+
     loop {}
 }
 
@@ -28,4 +34,11 @@ fn panic(_info: &PanicInfo) -> ! {
     log::error!("{PANIC_HEADER}");
     log::error!("{_info}");
     loop {}
+}
+
+fn check_memory(memory_regions: &MemoryRegions) {
+    for memory_region in memory_regions.deref() {
+        // log::info!("{}", memory_region.start)
+        log::info!("Detected memory region: [{:?}] 0x{:x} - 0x{:x} (0x{:x}b)", memory_region.kind, memory_region.start, memory_region.end, (memory_region.end - memory_region.start))
+    }
 }
